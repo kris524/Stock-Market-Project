adec38a5964a248e8d7a68ab42c17e284b0f12bc
Kristiyan Dilov <54142949+kris524@users.noreply.github.com>
2021-06-22 00:14:06 +0100

    Add files via upload

--- Web Scraping.py
+++ Web Scraping.py
@@ -6,14 +6,20 @@ from bs4 import BeautifulSoup
 import requests
 
 
-
+#stock market news article we want to scrape its text
 url = 'https://uk.finance.yahoo.com/news/automotive-volvo-signs-gigafactory-deal-swedish-electric-battery-maker-northvolt-103840931.html'
+
+#use requests module to get the HTML content and save it to the html_page variable
 res = requests.get(url)
 html_page = res.content
-soup = BeautifulSoup(html_page, 'html.parser')
-text = soup.find_all(text=True)
 
+#create a soup object
+soup = BeautifulSoup(html_page, 'html5lib') #html.parser
 
-print(soup.get_text())
+text = soup.find_all('div',class_='caas-content-wrapper')
+#text = soup.find_all(text=True)
+
+print(text)
+#print(soup.get_text())
 
 #print(output)
